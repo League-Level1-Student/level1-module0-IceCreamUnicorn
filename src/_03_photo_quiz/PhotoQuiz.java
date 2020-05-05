c70b6025db46fecf6357e4d685402126ba1b3954
Joseph <Joseph@192.168.0.54>
2020-05-04 19:57:14 -0700

    Drum kit

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -25,36 +25,50 @@ public class PhotoQuiz {
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
-		// “Copy Image Address”)
+		// â€œCopy Image Addressâ€�)
 
 		// 2. create a variable of type "Component" that will hold your image
-
+		String image = "https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Bob_the_builder.jpg/220px-Bob_the_builder.jpg";
 		// 3. use the "createImage()" method below to initialize your Component
-
+		Component name = createImage(image);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(name);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String Bob = JOptionPane.showInputDialog("Who is the character on this image?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(Bob.equals("Bob the builder"))
+		{
+			JOptionPane.showMessageDialog(null,"CORRECT");
+		}
+		else
+		{
+			JOptionPane.showInputDialog("INCORRECT");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(name);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		String image2 = "https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg";
 		// 11. add the second image to the quiz window
-
+		Component name2 = createImage(image2);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String question = JOptionPane.showInputDialog("What am I showing.");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+				if(question.equals("computer"))
+				{
+					JOptionPane.showMessageDialog(null,"CORRECT");
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null,"INCORRECT");
+				}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
